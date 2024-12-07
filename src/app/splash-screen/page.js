077c15c5d739c8878ec0052ba3647900fb5cdaa4
harng <wangth94@gmail.com>
2024-12-07 20:22:24 +0800

    commit

--- src/app/splash-screen/page.js
+++ src/app/splash-screen/page.js
@@ -8,9 +8,9 @@ function SplashScreen() {
             <Image
                 className="logo-img"
                 alt=""
-                width={1920}
-                height={1080}
-                src="https://resources.wobbjobs.com/companies/2ee9473f-23ff-4bd3-acff-c9d281ac23d2/company_logo/original/289f98b8-3690-420e-8c6e-6fa240233ad1-1168x305_IntrixLogo.png"
+                width={400}
+                height={180}
+                src="/logo.png"
             />
         </div>
     );
