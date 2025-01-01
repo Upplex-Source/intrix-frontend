4a519e975570d1824a8e745d2f20fc8bbdd57e44
huiyaopuah <huiyao_1201@hotmail.com>
2025-01-01 14:59:34 +0800

    Commit: add splash screen

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,33 @@
 "use client"; // Must be a Client Component
-import { Suspense } from 'react';
+import { Suspense, useEffect } from "react";
 import Header from "@/components/header/Header";
 import Footer from "@/components/Footer";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import Loader from "@/components/Loader";
+import SplashScreen from "./splash-screen/SplashScreen";
 
 export default function Layout({ children }) {
     const router = useRouter();
     const pathname = usePathname();
     const [isVisible, setIsVisible] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+    }, []);
+
+    if (isLoading) {
+        return <SplashScreen />;
+    }
+
     return (
         <>
             <Header isVisible={isVisible} setIsVisible={setIsVisible} />
             <Suspense fallback={<Loader />}>
-                <div
-                    className={`transition-all duration-500 ${
-                        isVisible ? "mt-[90px] md:mt-[155px]" : "mt-[50px] md:mt-[120px]"
-                    }`}
-                >
-                    {children}
-                </div>
+                <div className={`transition-all duration-500 ${isVisible ? "mt-[90px] md:mt-[155px]" : "mt-[50px] md:mt-[120px]"}`}>{children}</div>
             </Suspense>
             {["/", "/test", "/", "/discover"].includes(pathname) ? null : <Footer />}
         </>
