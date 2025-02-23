85b839e14636aa090aac8e30d2d2f91e040af048
yeemay <yeemay@hotmail.com>
2025-02-23 21:48:33 +0800

    Commit: Adjust mobile styling for explore overview page

--- src/components/explore/TitaniumInnerCore.js
+++ src/components/explore/TitaniumInnerCore.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
 
-
 const TitaniumInnerCore = () => {
     return (
-        <div className="mx-auto relative">
+        <div className="w-full mx-auto relative">
             <Image
                 src="/explore/titanium-bg.png" // Replace with the image URL or leave empty for now
                 alt="Intrix Experience Centre"
@@ -11,19 +10,16 @@ const TitaniumInnerCore = () => {
                 width={2888} // Adjust width and height
                 height={1774} // For placeholder purposes
             />
-            <div className="absolute inset-y-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent">
-                <div className="min-[1600px]:container min-[1600px]:mx-auto flex flex-col py-6 px-12 lg:py-12 lg:px-24 justify-center leading-[1.2] text-white w-2/3 h-full">
-                    <p className="text-[#F7F7F7] text-[30px] font-bold mb-4 max-w-[450px]">
-                        The World&apos;s First and Only
-                        Titanium Inner Core
-                    </p>
+            <div className="w-full absolute inset-y-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent">
+                <div className="w-full min-[1600px]:container min-[1600px]:mx-auto flex flex-col py-6 px-4 lg:py-12 lg:px-24 justify-center leading-[1.2] text-white w-2/3 h-full">
+                    <p className="text-[#F7F7F7] text-[30px] font-bold mb-4 max-w-[450px]">The World&apos;s First and Only Titanium Inner Core</p>
                     <p className="text-[#F7F7F7] text-[1em] mb-4 lg:mb-6 xl:mb-12 max-w-[500px]">
-                        Experience the future of innovation with our patented technology:
-                        the Titanium Inner Core — where strength and safety meets precision.
+                        Experience the future of innovation with our patented technology: the Titanium Inner Core — where strength and safety meets
+                        precision.
                     </p>
                     <div className="flex flex-col px-6 space-y-6">
                         <div className="flex w-full items-center gap-x-6">
-                            <Image className="" src={'/explore/anticorrosion.png'} alt="" width={60} height={60} />
+                            <Image className="" src={"/explore/anticorrosion.png"} alt="" width={60} height={60} />
                             <div className="flex flex-col w-4/5">
                                 <p className="text-white text-[1em] xl:text-[1.3em] font-bold mb-2">Anti-Corrosion & Anti-Limescale</p>
                                 <p className="text-white text-[0.8em] xl:text-[1em] font-[Montserrat-Regular]">
@@ -34,7 +30,7 @@ const TitaniumInnerCore = () => {
                             </div>
                         </div>
                         <div className="flex w-full items-center gap-x-6">
-                            <Image className="px-2" src={'/explore/strength.png'} alt="" width={60} height={60} />
+                            <Image className="px-2" src={"/explore/strength.png"} alt="" width={60} height={60} />
                             <div className="flex flex-col w-4/5">
                                 <p className="text-white text-[1em] xl:text-[1.3em] font-bold mb-2">High Strength & Durability</p>
                                 <p className="text-white text-[0.8em] xl:text-[1em] font-[Montserrat-Regular]">
@@ -45,7 +41,7 @@ const TitaniumInnerCore = () => {
                             </div>
                         </div>
                         <div className="flex w-full items-center gap-x-6">
-                            <Image className="px-1" src={'/explore/heat_transfer.png'} alt="" width={60} height={60} />
+                            <Image className="px-1" src={"/explore/heat_transfer.png"} alt="" width={60} height={60} />
                             <div className="flex flex-col w-4/5">
                                 <p className="text-white text-[1em] xl:text-[1.3em] font-bold mb-2">Efficient Heat Transfer</p>
                                 <p className="text-white text-[0.8em] xl:text-[1em] font-[Montserrat-Regular]">
@@ -57,7 +53,7 @@ const TitaniumInnerCore = () => {
                             </div>
                         </div>
                         <div className="flex w-full items-center gap-x-6">
-                            <Image className="" src={'/explore/biopcompatible.png'} alt="" width={60} height={60} />
+                            <Image className="" src={"/explore/biopcompatible.png"} alt="" width={60} height={60} />
                             <div className="flex flex-col w-4/5">
                                 <p className="text-white text-[1em] xl:text-[1.3em] font-bold mb-2">Biocompatible</p>
                                 <p className="text-white text-[0.8em] xl:text-[1em] font-[Montserrat-Regular]">
@@ -67,7 +63,10 @@ const TitaniumInnerCore = () => {
                             </div>
                         </div>
                     </div>
-                    <i className="text-[11px] max-w-[400px] italic text-[#F7F7F7] mt-6 lg:mt-12">*Grade 1 Titanium is one of the softest and most ductile grades exhibiting the greatest formability, offering high impact toughness and resistance.</i>
+                    <i className="text-[11px] max-w-[400px] italic text-[#F7F7F7] mt-6 lg:mt-12">
+                        *Grade 1 Titanium is one of the softest and most ductile grades exhibiting the greatest formability, offering high impact
+                        toughness and resistance.
+                    </i>
                 </div>
             </div>
         </div>
