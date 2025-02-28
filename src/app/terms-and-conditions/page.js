02287266e6f2d449cccccd16e0cace40132f02eb
harng <wangth94@gmail.com>
2025-02-28 21:12:02 +0800

    commit

--- src/app/terms-and-conditions/page.js
+++ src/app/terms-and-conditions/page.js
@@ -239,7 +239,7 @@ function Page() {
                                     +
                                 </span>
                             </button>
-                            <div className={`overflow-hidden transition-all duration-300 ${openIndex === index ? "max-h-[800px] pb-6" : "max-h-0"}`}>
+                            <div className={`overflow-hidden transition-all duration-300 ${openIndex === index ? "max-h-[5000px] pb-6" : "max-h-0"}`}>
                                 <div className="text-[16px] text-[#3C3C43D9] text-left" dangerouslySetInnerHTML={{ __html: tnc.answer }}></div>
                             </div>
                         </div>
