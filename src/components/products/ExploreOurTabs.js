e45e8eb5cab953b91ddfa4d0dc3e87f48e5ca6e9
huiyaopuah <huiyao.puah@itoswtech.com.my>
2025-05-15 14:07:43 +0800

    Commit: Work on change tap  image on select

--- src/components/products/ExploreOurTabs.js
+++ src/components/products/ExploreOurTabs.js
@@ -1,128 +1,130 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ColorDropdown from "./ColorDropdown";
 import { Swiper, SwiperSlide } from "swiper/react"; // Import React components
 import "swiper/css"; // Core Swiper CSS
+import Cookies from "js-cookie";
+import { getProducts } from "@/service/product-api/ProductService";
 
 const ExploreOurTabs = () => {
     const [openDropdown, setOpenDropdown] = useState(null);
+    const [productList, setProductList] = useState(null);
+
+    useEffect(() => {
+        const handleFirstLoad = async () => {
+            const session_key = Cookies.get("session_key");
+
+            const obj = {
+                session_key: session_key,
+                length: 10,
+                start: 0,
+            };
+
+            try {
+                const result = await getProducts(obj);
+
+                if (result) {
+                    setProductList(result.data);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        handleFirstLoad();
+    }, []);
+
     return (
         <div className="container mx-auto">
-                        <p className="text-[#343637] text-[24px] sm:text-[30px] min-[1441px]:text-[40px] text-center font-bold mb-6">Explore Our One Tap Series</p>
-                        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6"> */}
-                        <Swiper
-                            // loop={true} 
-                            slidesPerView={4}
-                            spaceBetween={20}
-                            className="my-swiper !pb-[60px]"
-                            breakpoints={{
-                            0: {
-                                slidesPerView: 1.2,
-                            },
-                            370: {
-                                slidesPerView: 1.4,
-                            },
-                            425: {
-                                slidesPerView: 1.8,
-                            },
-                            768: {
-                                slidesPerView: 2.4,
-                            },
-                            1024: {
-                                slidesPerView: 3.5,
-                            },
-                            1280: {
-                                slidesPerView: 4,
-                            },
-                            }}
-                        >
-                            <SwiperSlide className="text-[#421908] bg-white rounded-lg px-4 min-[375px]:px-6 pb-6">
-                                <Image src={'/explore/tap-1.png?v=1.1'} alt="" className="product-image" width={300} height={400} />
-                                    <p className="text-[14px] md:text-[16px]">INTRIX One Tap 5-in-1</p>
-                                    <p className="text-[#421908] text-[18px] md:text-[24px] font-bold">RM 7,500.00</p>
-                                    <p className="text-[12px] mb-6 h-[70px] lg:h-[65px]">
-                                    The ultimate tap with hot, ambient and chilled purified water. Available in Chrome & Matte Black.
-                                    </p>
-                                    <ColorDropdown 
-                                        allowedOptions={['Chrome', 'Matte Black']} 
-                                        isOpen={openDropdown === 1}
-                                        setOpenDropdown={() => setOpenDropdown(openDropdown === 1 ? null : 1)}
-                                    />
-                                    <Link
-                                        href={'/product/intrix-one-tap-5-in-1'}
-                                        className="mx-auto my-2 outline-0 relative w-full sm:w-fit send_now_btn bg-[#F79932] text-[#fff] font-[Mulish-Light] transition py-3 rounded-md flex items-center gap-x-4 pl-4 min-[375px]:pl-6 pr-10 min-[520px]:pr-24 lg:pr-20 xl:pr-24"
-                                    >
-                                        <span>Learn More</span> 
-                                        <Image className="absolute right-4 min-[520px]:right-8" src={'/product/arrow-right-white.png'} alt="arrow" width={25} height={25} />
-                                    </Link>
-                                    <Link href={'#'} className="hover:underline text-center w-fit mx-auto block outline-0 text-[12px]">Buy Now</Link>
-                            </SwiperSlide>
-                            <SwiperSlide className="text-[#421908] bg-white rounded-lg px-4 min-[375px]:px-6 pb-6">
-                                <Image src={'/explore/tap-3.png?v=1.1'} alt="" className="product-image" width={300} height={400} />
-                                    <p className="text-[14px] md:text-[16px]">INTRIX One Tap 4-in-1</p>
-                                    <p className="text-[#421908] text-[18px] md:text-[24px] font-bold">RM 5,200.00</p>
-                                    <p className="text-[12px] mb-6 h-[70px] lg:h-[65px]">
-                                    Perfect for hot to boiling and ambient purified water. Available in Chrome, Matte Black, Satin Gold & Gunmetal Grey.
-                                    </p>
-                                    <ColorDropdown 
-                                        allowedOptions={["Chrome", "Satin Gold", "Gunmetal Grey", "Matte Black"]}
-                                        isOpen={openDropdown === 2}
-                                        setOpenDropdown={() => setOpenDropdown(openDropdown === 2 ? null : 2)} 
-                                    />
-                                    <Link
-                                        href={'/product/intrix-one-tap-4-in-1'}
-                                        className="mx-auto my-2 outline-0 relative w-full sm:w-fit send_now_btn bg-[#F79932] text-[#fff] font-[Mulish-Light] transition py-3 rounded-md flex items-center gap-x-4 pl-4 min-[375px]:pl-6 pr-10 min-[520px]:pr-24 lg:pr-20 xl:pr-24"
-                                    >
-                                        <span>Learn More</span> 
-                                        <Image className="absolute right-4 min-[520px]:right-8" src={'/product/arrow-right-white.png'} alt="arrow" width={25} height={25} />
-                                    </Link>
-                                    <Link href={'#'} className="hover:underline text-center w-fit mx-auto block outline-0 text-[12px]">Buy Now</Link>
-                            </SwiperSlide>
-                            <SwiperSlide className="text-[#421908] bg-white rounded-lg px-4 min-[375px]:px-6 pb-6">
-                                <Image src={'/explore/tap-2.png?v=1.2'} alt="" className="product-image" width={300} height={400} />
-                                    <p className="text-[14px] md:text-[16px]">INTRIX One Tap 2-in-1</p>
-                                    <p className="text-[#421908] text-[18px] md:text-[24px] font-bold">RM 4,500.00</p>
-                                    <p className="text-[12px] mb-6 h-[70px] lg:h-[65px]">
-                                    Hot water (ranging from 60°C to 98°C) and ambient purified water straight from the tap. Available in Chrome & Matte Black.
-                                    </p>
-                                    <ColorDropdown 
-                                        allowedOptions={['Chrome', 'Matte Black']} 
-                                        isOpen={openDropdown === 3}
-                                        setOpenDropdown={() => setOpenDropdown(openDropdown === 3 ? null : 3)}
-                                    />
-                                    <Link
-                                        href={'/product/intrix-one-tap-2-in-1'}
-                                        className="mx-auto my-2 outline-0 relative w-full sm:w-fit send_now_btn bg-[#F79932] text-[#fff] font-[Mulish-Light] transition py-3 rounded-md flex items-center gap-x-4 pl-4 min-[375px]:pl-6 pr-10 min-[520px]:pr-24 lg:pr-20 xl:pr-24"
-                                    >
-                                        <span>Learn More</span> 
-                                        <Image className="absolute right-4 min-[520px]:right-8" src={'/product/arrow-right-white.png'} alt="arrow" width={25} height={25} />
-                                    </Link>
-                                    <Link href={'#'} className="hover:underline text-center w-fit mx-auto block outline-0 text-[12px]">Buy Now</Link>
-                            </SwiperSlide>
-                            <SwiperSlide className="text-[#421908] bg-white rounded-lg px-4 min-[375px]:px-6 pb-6">
-                                <Image src={'/explore/tap-4.png?v=1.1'} alt="" className="product-image" width={300} height={400} />
-                                    <p className="text-[14px] md:text-[16px]">INTRIX One Tap Lite</p>
-                                    <p className="text-[#421908] text-[18px] md:text-[24px] font-bold">RM 3,988.00</p>
-                                    <p className="text-[12px] mb-6 h-[70px] lg:h-[65px]">
-                                    Great for hot (preset to 98​​°C) and ambient purified water straight from the tap. Available in Chrome.
-                                    </p>
-                                    <ColorDropdown 
-                                        allowedOptions={['Chrome']} 
-                                        isOpen={openDropdown === 4}
-                                        setOpenDropdown={() => setOpenDropdown(openDropdown === 4 ? null : 4)}
-                                    />
-                                    <Link
-                                        href={'/product/intrix-one-tap-lite'}
-                                        className="mx-auto my-2 outline-0 relative w-full sm:w-fit send_now_btn bg-[#F79932] text-[#fff] font-[Mulish-Light] transition py-3 rounded-md flex items-center gap-x-4 pl-4 min-[375px]:pl-6 pr-10 min-[520px]:pr-24 lg:pr-20 xl:pr-24"
-                                    >
-                                        <span>Learn More</span> 
-                                        <Image className="absolute right-4 min-[520px]:right-8" src={'/product/arrow-right-white.png'} alt="arrow" width={25} height={25} />
-                                    </Link>
-                                    <Link href={'#'} className="hover:underline text-center w-fit mx-auto block outline-0 text-[12px]">Buy Now</Link>
-                            </SwiperSlide>
-                        </Swiper>
-                    </div>
+            <p className="text-[#343637] text-[24px] sm:text-[30px] min-[1441px]:text-[40px] text-center font-bold mb-6">
+                Explore Our One Tap Series
+            </p>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={20}
+                className="my-swiper !pb-[60px]"
+                breakpoints={{
+                    0: {
+                        slidesPerView: 1.2,
+                    },
+                    370: {
+                        slidesPerView: 1.4,
+                    },
+                    425: {
+                        slidesPerView: 1.8,
+                    },
+                    768: {
+                        slidesPerView: 2.4,
+                    },
+                    1024: {
+                        slidesPerView: 3.5,
+                    },
+                    1280: {
+                        slidesPerView: 4,
+                    },
+                }}
+            >
+                {productList?.map((product, index) => {
+                    function titleCase(str) {
+                        var splitStr = str.toLowerCase().split(" ");
+                        for (var i = 0; i < splitStr.length; i++) {
+                            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+                        }
+                        return splitStr.join(" ");
+                    }
+
+                    let filteredValues = product.colors.reduce((results, el) => {
+                        results.push(titleCase(el.title));
+                        return results;
+                    }, []);
+
+                    function handleSelect(color, productId) {
+                        let newImg = product.active_product_variants.find((item) => item.title === color.toUpperCase());
+
+                        document.getElementById(`tap-${productId}`).srcset = newImg.image_path;
+                    }
+
+                    return (
+                        <SwiperSlide key={index} className="text-[#421908] bg-white rounded-lg px-4 min-[375px]:px-6 pb-6">
+                            <Image
+                                id={`tap-${product.id}`}
+                                src={product.active_product_variants[0].image_path}
+                                alt=""
+                                className="product-image"
+                                width={300}
+                                height={400}
+                            />
+                            <p className="text-[14px] md:text-[16px]">{product.title}</p>
+                            <p className="text-[#421908] text-[18px] md:text-[24px] font-bold">RM {product.price}</p>
+                            <p className="text-[12px] mb-6 h-[70px] lg:h-[65px]">{product.description}</p>
+                            <ColorDropdown
+                                allowedOptions={filteredValues}
+                                isOpen={openDropdown === product.id}
+                                setOpenDropdown={() => setOpenDropdown(openDropdown === product.id ? null : product.id)}
+                                handleSelect={handleSelect}
+                                productId={product.id}
+                            />
+                            <Link
+                                href={"/product/intrix-one-tap-5-in-1"}
+                                className="mx-auto my-2 outline-0 relative w-full sm:w-fit send_now_btn bg-[#F79932] text-[#fff] font-[Mulish-Light] transition py-3 rounded-md flex items-center gap-x-4 pl-4 min-[375px]:pl-6 pr-10 min-[520px]:pr-24 lg:pr-20 xl:pr-24"
+                            >
+                                <span>Learn More</span>
+                                <Image
+                                    className="absolute right-4 min-[520px]:right-8"
+                                    src={"/product/arrow-right-white.png"}
+                                    alt="arrow"
+                                    width={25}
+                                    height={25}
+                                />
+                            </Link>
+                            <Link href={"#"} className="hover:underline text-center w-fit mx-auto block outline-0 text-[12px]">
+                                Buy Now
+                            </Link>
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+        </div>
     );
 };
 
